0f9d2002121ceb8ec689c90d06293b035a1713df
Ethal <developer@ethal-consulting.com>
2016-03-20 23:47:30 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+      name='calcul',
+      version='0.01',
+      description='calcul',
+      author='Ethal',
+      author_email='developer@ethal-consulting.com',
+      url='https://github.com/ethal/calcul',
